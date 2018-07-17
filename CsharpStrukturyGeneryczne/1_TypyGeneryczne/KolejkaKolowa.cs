9dc3c1d637ed0fe270b2c057fa6b769d05f6f286
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-17 11:06:07 +0200

    1_3 NaszaKolekcja

---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,60 @@
+﻿namespace _1_TypyGeneryczne
+{
+    public class KolejkaKolowa
+    {
+        private double[] bufor;
+        private int pocztekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowa() : this(pojemnosc: 5)
+        {
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new double[pojemnosc + 1];
+            pocztekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(double wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == pocztekBufora)
+                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+        }
+
+        public double Czytaj()
+        {
+            var wynik = bufor[pocztekBufora];
+            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == pocztekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
+            }
+        }
+    }
+}
