de3074c6bb6929f02e48bd57e4a464e4b9900b6c
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-18 11:02:04 +0200

    1_8 Kopiuj Wklej bardzo złe rozwiązanie

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
@@ -2,7 +2,7 @@
 {
     public class KolejkaKolowa
     {
-        private object[] bufor;
+        private double[] bufor;
         private int pocztekBufora;
         private int koniecBufora;
 
@@ -12,12 +12,12 @@
 
         public KolejkaKolowa(int pojemnosc)
         {
-            bufor = new object[pojemnosc + 1];
+            bufor = new double[pojemnosc + 1];
             pocztekBufora = 0;
             koniecBufora = 0;
         }
 
-        public void Zapisz(object wartosc)
+        public void Zapisz(double wartosc)
         {
             bufor[koniecBufora] = wartosc;
             koniecBufora = (koniecBufora + 1) % bufor.Length;
@@ -26,7 +26,123 @@
                 pocztekBufora = (pocztekBufora + 1) % bufor.Length;
         }
 
-        public object Czytaj()
+        public double Czytaj()
+        {
+            var wynik = bufor[pocztekBufora];
+            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == pocztekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
+            }
+        }
+    }
+
+    public class KolejkaKolowaString
+    {
+        private string[] bufor;
+        private int pocztekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowaString() : this(pojemnosc: 5)
+        {
+        }
+
+        public KolejkaKolowaString(int pojemnosc)
+        {
+            bufor = new string[pojemnosc + 1];
+            pocztekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(string wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == pocztekBufora)
+                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+        }
+
+        public string Czytaj()
+        {
+            var wynik = bufor[pocztekBufora];
+            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == pocztekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
+            }
+        }
+    }
+
+    public class KolejkaKolowaInt
+    {
+        private int[] bufor;
+        private int pocztekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowaInt() : this(pojemnosc: 5)
+        {
+        }
+
+        public KolejkaKolowaInt(int pojemnosc)
+        {
+            bufor = new int[pojemnosc + 1];
+            pocztekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(int wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == pocztekBufora)
+                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+        }
+
+        public int Czytaj()
         {
             var wynik = bufor[pocztekBufora];
             pocztekBufora = (pocztekBufora + 1) % bufor.Length;
