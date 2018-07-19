9a05981a14544ed71eb9e97e366f80de2a1a9295
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-19 09:52:01 +0200

    1_10 KolejkaKolowa<T> - generyczna

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
@@ -1,8 +1,8 @@
 ﻿namespace _1_TypyGeneryczne
 {
-    public class KolejkaKolowa
+    public class KolejkaKolowa<T>
     {
-        private double[] bufor;
+        private T[] bufor;
         private int poczatekBufora;
         private int koniecBufora;
 
@@ -12,12 +12,12 @@
 
         public KolejkaKolowa(int pojemnosc)
         {
-            bufor = new double[pojemnosc + 1];
+            bufor = new T[pojemnosc + 1];
             poczatekBufora = 0;
             koniecBufora = 0;
         }
 
-        public void Zapisz(double wartosc)
+        public void Zapisz(T wartosc)
         {
             bufor[koniecBufora] = wartosc;
             koniecBufora = (koniecBufora + 1) % bufor.Length;
@@ -26,7 +26,7 @@
                 poczatekBufora = (poczatekBufora + 1) % bufor.Length;
         }
 
-        public double Czytaj()
+        public T Czytaj()
         {
             var wynik = bufor[poczatekBufora];
             poczatekBufora = (poczatekBufora + 1) % bufor.Length;
@@ -54,122 +54,6 @@
             get
             {
                 return (koniecBufora + 1) % bufor.Length == poczatekBufora;
-            }
-        }
-    }
-
-    public class KolejkaKolowaString
-    {
-        private string[] bufor;
-        private int pocztekBufora;
-        private int koniecBufora;
-
-        public KolejkaKolowaString() : this(pojemnosc: 5)
-        {
-        }
-
-        public KolejkaKolowaString(int pojemnosc)
-        {
-            bufor = new string[pojemnosc + 1];
-            pocztekBufora = 0;
-            koniecBufora = 0;
-        }
-
-        public void Zapisz(string wartosc)
-        {
-            bufor[koniecBufora] = wartosc;
-            koniecBufora = (koniecBufora + 1) % bufor.Length;
-
-            if (koniecBufora == pocztekBufora)
-                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
-        }
-
-        public string Czytaj()
-        {
-            var wynik = bufor[pocztekBufora];
-            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
-            return wynik;
-        }
-
-        public int Pojemnosc
-        {
-            get
-            {
-                return bufor.Length;
-            }
-        }
-
-        public bool JestPusty
-        {
-            get
-            {
-                return koniecBufora == pocztekBufora;
-            }
-        }
-
-        public bool JestPelny
-        {
-            get
-            {
-                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
-            }
-        }
-    }
-
-    public class KolejkaKolowaInt
-    {
-        private int[] bufor;
-        private int pocztekBufora;
-        private int koniecBufora;
-
-        public KolejkaKolowaInt() : this(pojemnosc: 5)
-        {
-        }
-
-        public KolejkaKolowaInt(int pojemnosc)
-        {
-            bufor = new int[pojemnosc + 1];
-            pocztekBufora = 0;
-            koniecBufora = 0;
-        }
-
-        public void Zapisz(int wartosc)
-        {
-            bufor[koniecBufora] = wartosc;
-            koniecBufora = (koniecBufora + 1) % bufor.Length;
-
-            if (koniecBufora == pocztekBufora)
-                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
-        }
-
-        public int Czytaj()
-        {
-            var wynik = bufor[pocztekBufora];
-            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
-            return wynik;
-        }
-
-        public int Pojemnosc
-        {
-            get
-            {
-                return bufor.Length;
-            }
-        }
-
-        public bool JestPusty
-        {
-            get
-            {
-                return koniecBufora == pocztekBufora;
-            }
-        }
-
-        public bool JestPelny
-        {
-            get
-            {
-                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
             }
         }
     }
--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -6,30 +6,53 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
-            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+            var kolejkaInt = new KolejkaKolowa<int>();
+            var kolejkaString = new KolejkaKolowa<string>(1000);
 
-            while (true)
+            var kolejkaOsob = new KolejkaKolowa<Osoba>();
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Tomek", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Kacpek", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
+      
+
+
+            while (!kolejkaOsob.JestPusty)
             {
-                var wartosc = 0.0;
-                var wartoscWejsciowa = Console.ReadLine();
-
-                if (double.TryParse(wartoscWejsciowa, out wartosc))
-                {
-                    kolejka.Zapisz(wartosc);
-                    continue;
-                }
-                break;
+                var wynik = kolejkaOsob.Czytaj().Imie;
+                Console.WriteLine(wynik);
             }
 
-            var suma = 0.0;
-            Console.WriteLine("W naszej kolejce jest :");
+            //while (true)
+            //{
+            //    var wartosc = 0.0;
+            //    var wartoscWejsciowa = Console.ReadLine();
 
-            while (!kolejka.JestPusty)
-            {
-                suma += kolejka.Czytaj();
-            }
-            Console.WriteLine(suma);
+            //    if (double.TryParse(wartoscWejsciowa, out wartosc))
+            //    {
+            //        kolejka.Zapisz(wartosc);
+            //        continue;
+            //    }
+            //    break;
+            //}
+
+            //var suma = 0.0;
+            //Console.WriteLine("W naszej kolejce jest :");
+
+            //while (!kolejka.JestPusty)
+            //{
+            //    suma += kolejka.Czytaj();
+            //}
+            //Console.WriteLine(suma);
 
         }
+    }
+
+    public class Osoba
+    {
+        public string Imie { get; set; }
+
+        public string Nazwisko { get; set; }
     }
 }
