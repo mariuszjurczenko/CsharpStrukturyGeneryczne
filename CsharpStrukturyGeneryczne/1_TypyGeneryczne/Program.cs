3c5d1a9d3715644d6aed6b9d76bf0dcd072e5547
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-19 12:00:03 +0200

    1_11 Terminologia

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -6,53 +6,39 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
-            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
-            var kolejkaInt = new KolejkaKolowa<int>();
-            var kolejkaString = new KolejkaKolowa<string>(1000);
+            var kolejka = new KolejkaKolowa<double>();
 
-            var kolejkaOsob = new KolejkaKolowa<Osoba>();
-            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
-            kolejkaOsob.Zapisz(new Osoba { Imie = "Tomek", Nazwisko = "Nowak" });
-            kolejkaOsob.Zapisz(new Osoba { Imie = "Kacpek", Nazwisko = "Nowak" });
-            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
-      
+            WprowadzanieDanych(kolejka);
 
-
-            while (!kolejkaOsob.JestPusty)
-            {
-                var wynik = kolejkaOsob.Czytaj().Imie;
-                Console.WriteLine(wynik);
-            }
-
-            //while (true)
-            //{
-            //    var wartosc = 0.0;
-            //    var wartoscWejsciowa = Console.ReadLine();
-
-            //    if (double.TryParse(wartoscWejsciowa, out wartosc))
-            //    {
-            //        kolejka.Zapisz(wartosc);
-            //        continue;
-            //    }
-            //    break;
-            //}
-
-            //var suma = 0.0;
-            //Console.WriteLine("W naszej kolejce jest :");
-
-            //while (!kolejka.JestPusty)
-            //{
-            //    suma += kolejka.Czytaj();
-            //}
-            //Console.WriteLine(suma);
-
+            PrzetwarzanieDanych(kolejka);
         }
-    }
 
-    public class Osoba
-    {
-        public string Imie { get; set; }
+        private static void PrzetwarzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("W naszej kolejce jest :");
 
-        public string Nazwisko { get; set; }
+            while (!kolejka.JestPusty)
+            {
+                suma += kolejka.Czytaj();
+            }
+            Console.WriteLine(suma);
+        }
+
+        private static void WprowadzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+        }
     }
 }
