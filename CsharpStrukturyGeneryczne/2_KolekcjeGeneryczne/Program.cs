28cd26897415163433f5d85e04c3aacc01b0d3dc
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-20 08:40:19 +0200

    2_1 Wprowadzenie

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2_KolekcjeGeneryczne
 {
@@ -10,6 +7,25 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Pracownik[] pracownicy = new Pracownik[]
+            {
+                new Pracownik { Imie = "Marcin", Nazwisko="Nowak"},
+                new Pracownik { Imie = "Jan", Nazwisko="Nowak"},
+                new Pracownik { Imie = "Tomek", Nazwisko="Nowak"},
+            };
+
+            foreach (var pracownik in pracownicy)
+            {
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+            for (int i = 0; i < pracownicy.Length; i++)
+            {
+                Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
+            }
+
+            Array.Resize(ref pracownicy, 10);
+            pracownicy[9] = new Pracownik { Imie = "Anatol", Nazwisko="Koszyk"};
         }
     }
 }
