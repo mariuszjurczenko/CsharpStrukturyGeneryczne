2f59dd132fbee457ed0ee08633c3a75f04b88db6
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-20 10:11:25 +0200

    2_3 Lista Testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44d6d0aa-126c-4303-bef5-09f67b67f1e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
