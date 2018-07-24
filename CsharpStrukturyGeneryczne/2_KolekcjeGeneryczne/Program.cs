fcfdf46c4b420c9ad7a4be4c7d4595ac9753a8a6
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-24 08:41:36 +0200

    2_6 Stos

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -13,12 +13,28 @@ namespace _2_KolekcjeGeneryczne
             kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
             kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
 
-            while(kolejka.Count > 0)
+            Console.WriteLine("Kolejka");
+
+            while (kolejka.Count > 0)
             {
                 var pracownik = kolejka.Dequeue();
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
 
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Tomek", Nazwisko = "Kowak" });
+            stos.Push(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
+            stos.Push(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
+
+            Console.WriteLine();
+            Console.WriteLine("Stos");
+
+            while (stos.Count > 0)
+            {
+                var pracownik = stos.Pop();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
         }
     }
 }
