76377cb8ca03160d398f6a811c7c9ec5df63c46d
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-07-27 09:00:47 +0200

    2_15 SortedList

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -13,8 +13,28 @@ namespace _2_KolekcjeGeneryczne
             //LinkedList();
             //LinkedList2();
             //Dictionary();
+            //SortedDictionary();
+            //SortedList();
+        }
 
-            var pracownicy = new SortedDictionary<string, List<Pracownik>>();
+        private static void SortedList()
+        {
+            var listaPosortowana = new SortedList<int, string>();
+
+            listaPosortowana.Add(3, "trzy");
+            listaPosortowana.Add(1, "jeden");
+            listaPosortowana.Add(4, "cztery");
+            listaPosortowana.Add(2, "dwa");
+
+            foreach (var item in listaPosortowana)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+
+        private static void SortedDictionary()
+        {
+            var pracownicy = new SortedList<string, List<Pracownik>>();
 
             pracownicy.Add("Sprzedaż", new List<Pracownik> { new Pracownik { Imie = "Jan", Nazwisko = "Kowal" },
                                                              new Pracownik { Imie = "Tomek", Nazwisko = "Nowak" },
@@ -32,7 +52,6 @@ namespace _2_KolekcjeGeneryczne
             {
                 Console.WriteLine("Ilość pracowników w dziale {0} wynosi {1}", item.Key, item.Value.Count);
             }
-            
         }
 
         private static void Dictionary()
