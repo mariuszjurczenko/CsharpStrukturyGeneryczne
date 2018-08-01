d8418dff64be56ab7b5ae1d8a093c5b5522ce7f5
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-08-01 10:49:24 +0200

    4 Metody Delegaty Generyczne

---
+++ CsharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _4_MetodyDelegatyGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kolejka = new KolejkaKolowa<double>();
+
+            WprowadzanieDanych(kolejka);
+
+            foreach (var item in kolejka)
+            {
+                Console.WriteLine(item);
+            }
+
+            PrzetwarzanieDanych(kolejka);
+        }
+
+        private static void PrzetwarzanieDanych(IKolejka<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("W naszej kolejce jest :");
+
+            while (!kolejka.JestPusty)
+            {
+                suma += kolejka.Czytaj();
+            }
+            Console.WriteLine(suma);
+        }
+
+        private static void WprowadzanieDanych(IKolejka<double> kolejka)
+        {
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
